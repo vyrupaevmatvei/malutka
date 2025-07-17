44d2bb8c101043ca4542a45141ed91a2ac5f657a
Impresum <vyrupaevmatvei@yandex.ru>
2025-07-17 14:41:56 +0300

    Add files via upload

--- zombie-generator.sh
+++ zombie-generator.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
-#Случайноечислоот1до10
-NUM=$((RANDOM%10+1))
-echo"Создаём$NUMзомби-процессов"
-for i in $(seq1$NUM); do
-#Создаёмдочернийпроцесс
-(
-    #ВнутридочернегопроцессазапускаемкороткийC-код,создающийзомби
-    gcc - x c -o /tmp/zombie $i - <<EOF 
-#include<unistd.h>
-#include<stdlib.h>
-intmain(){
-    pid_tpid=fork();
-    if(pid==0){
-        _exit(0);//потомокзавершается,становитсязомби}
-        else{
-        sleep(600); //родитель"забывает"вызватьwait}
-        return0;}EOF/tmp/zombie$i &
-)
+# Random number between 1 and 10
+NUM=$((RANDOM % 10 + 1))
+echo "Creating $NUM zombie processes"
+
+for i in $(seq 1 $NUM); do
+    # Create unique filename for each process
+    BIN_FILE=$(mktemp /tmp/zombie_XXXXXX)
+    
+    # Compile and run C program in background
+    gcc -x c -o "$BIN_FILE" - <<'EOF' && "$BIN_FILE" &
+#include <unistd.h>
+#include <stdlib.h>
+
+int main() {
+    pid_t pid = fork();
+    if (pid == 0) {
+        _exit(0);  // Child exits immediately → becomes zombie
+    } else {
+        sleep(600);  // Parent sleeps 10 minutes (doesn't call wait)
+    }
+    return 0;
+}
+EOF
 done
-        echo"Проверьтеzombi-процессыспомощьюкоманды:psaux|grepZ"
+
+echo "Check zombie processes using: ps aux | grep 'Z'"
